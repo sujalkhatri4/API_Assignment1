9c85f7d7ee6dc289e54ed42a72925b36d5ddc93e
Sujal <sujalkhatri411@gmail.com>
2024-09-29 15:59:56 -0400

    added express mongoose

---
+++ recipes-api/src/index.js
@@ -0,0 +1,9 @@
+// File: index.js
+// Student’s Name: Sujal
+// StudentID: 200598524
+// Date: 29 September
+
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const recipeRoutes = require('./routes/recipeRoutes');
