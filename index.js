5b69508120e6173e903cde8ee692f7dc71c30d2a
Sujal <sujalkhatri411@gmail.com>
2024-09-29 19:05:58 -0400

    added recipeRouter

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 const express = require('express')
 const  bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const recipesRouter = require('./routes/recipes'); 
+
 
 
 //express app inistialize
