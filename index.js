e3875b151e173f29ee83798905bfa1bef4451197
Sujal <sujalkhatri411@gmail.com>
2024-09-29 04:41:03 -0400

    line added to index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 //import express
 const express = require('express')
+
+//express app inistialize
+const app =express();
+
+//set the port
+const  port = 3000;
+
+// start the server
+app.listen(port,()=>{
+    console.log(`the server is running on http://localhost:${port}`)
+});
