c832e5cdb2056e1d0e0d224f30ddc2291da4a2ff
Sujal <sujalkhatri411@gmail.com>
2024-09-29 11:53:25 -0400

    many changes that is added body parser to main file

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 //import express
 const express = require('express')
+const  bodyParser = require('body parser');
+
 
 //express app inistialize
 const app =express();
+
+//Midleware to parser json body
+app.use(bodyParser.json());
+app.use (bodyParser.urlencoded({extended:true}));
+
+//define a route
+app.get('/',(req,res)=>{
+    res.send('Welcome to my API ASSIGNMENT')
+})
 
 //set the port
 const  port = 3000;
