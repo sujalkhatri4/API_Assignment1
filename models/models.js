52a7fb7172522426ccc0e4c01bdc7d53c408bcdc
Sujal <sujalkhatri411@gmail.com>
2024-09-29 18:47:30 -0400

    model.js added

---
+++ models/models.js
@@ -0,0 +1,22 @@
+// models/recipe.js
+// Student’s Name: Sujal
+// StudentID: 200598524@student.georgianc.on.ca
+// Date: 29 September
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+    recipeName: { type: String, required: true },
+    ingredients0: { type: [String], required: true },
+    ingredients1: { type: [String], required: true },
+    ingredients2: { type: [String], required: true },
+    ingredients3: { type: [String], required: true },
+    ingredients4: { type: [String], required: true },
+    cookingTime: { type: Number, required: true },
+    difficulty: { type: String, required: true },
+    cuisine: { type: String, required: true },
+    description: { type: String, required: true },
+    photoLink: { type: String, required: true },
+    averageRating: { type: Number, default: 0 },
+});
+
+module.exports = mongoose.model('Recipe', recipeSchema);
