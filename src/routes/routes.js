b15e8486abd0f700fa2e9060dd5d48301c7a7f58
Sujal <sujalkhatri411@gmail.com>
2024-10-10 17:06:59 -0400

    routes to get all movies in routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const movieController = require('../controllers/Controller');
 const {validateMovie}=require('../middleware/middleware');
 
-//Route to import movies
+//route to import movies
 router.post('/import',movieController.importMovies);
+
+//route to get all movies
+router.get('/',movieController.getMovies);
