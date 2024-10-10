484fea94d1f5640d5fe398f05cd37da7f77c0279
Sujal <sujalkhatri411@gmail.com>
2024-10-10 16:58:47 -0400

    function added to import movies

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -42,3 +42,58 @@ exports.getMovieById = async(req,res) =>{
         res.status(500).send('Error retrieving the Movies');
     }
     };
+    // UPDATED MOVIE
+
+
+    //Get a single movie by Id
+exports.updateMovie = async(req,res) =>{
+    try{
+        const updatedMovie = await Movie.findByIdAndUpdate(req.params.id,req.body,{new:true});
+        if(!updatedMovie){
+            return res.status(404).send('Movie is not updated');
+        }
+        res.status(201).json(updatedMovie);
+    
+    }
+    catch(e){
+        console.error(e);
+        res.status(500).send('Error uodating the Movies');
+    }
+    };
+
+       //Delete a single movie by Id
+exports.deleteMovie = async(req,res) =>{
+    try{
+        const deletedMovie = await Movie.findByIdAndDelete(req.params.id);
+        if(!deletedMovie){
+            return res.status(404).send('Movie not found');
+        }
+        res.status(201).json(deletedMovie);
+    
+    }
+    catch(e){
+        console.error(e);
+        res.status(500).send('Error deleting the Movies');
+    }
+    };
+
+        // Function to import movies (moved from index.js)
+exports.importMovies = async (req, res) => {
+    try {
+        const data = JSON.parse(fs.readFileSync('./movies.json', 'utf-8')); // Read data from movies.json
+        const count = await Movie.countDocuments();
+        if (count === 0) {
+            await Movie.create(data); // Create movies in the database
+            console.log('Data successfully imported to MongoDb');
+            res.status(200).send('Data successfully imported');
+        } else {
+            res.status(200).send('Data already exists, skipping import');
+        }
+    } catch (e) {
+        console.error('Error importing data', e);
+        res.status(500).send('Error importing data');
+    }
+};
+
+
+
