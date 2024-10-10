8a3de6cc98d46b831ffa33f565162ae057fe8471
Sujal <sujalkhatri411@gmail.com>
2024-10-10 16:56:11 -0400

    movie id added to controller.js

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -27,3 +27,18 @@ exports.createMovie = async(req,res)=>{
     }
 };
 
+//Get a single movie by Id
+exports.getMovieById = async(req,res) =>{
+    try{
+        const movie = await Movie.findById(req.params.id);
+        if(!movie){
+            return res.status(404).send('Movie is not found');
+        }
+        res.status(201).json(movie);
+    
+    }
+    catch(e){
+        console.error(e);
+        res.status(500).send('Error retrieving the Movies');
+    }
+    };
