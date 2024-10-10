4c2f72ae955b914dd137f11b9dd8a889c0bab67e
Sujal <sujalkhatri411@gmail.com>
2024-10-10 16:32:41 -0400

    schema for the movies

--- src/models/Movies.js
+++ src/models/Movies.js
@@ -1,28 +1,23 @@
-// models/recipe.js
-// Student’s Name: Sujal
-// StudentID: 200598524@student.georgianc.on.ca
-// Date: 29 September
-
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-//  the recipe schema
-const recipeSchema = new mongoose.Schema({
-    recipeName: { type: String, required: true },
-    ingredients0: { type: [String], required: true },
-    ingredients1: { type: [String], required: true },
-    ingredients2: { type: [String], required: true },
-    ingredients3: { type: [String], required: true },
-    ingredients4: { type: [String], required: true },
-    cookingTime: { type: Number, required: true },
-    difficulty: { type: String, required: true },
-    cuisine: { type: String, required: true },
-    description: { type: String, required: true },
-    photoLink: { type: String, required: true },
-    averageRating: { type: Number, default: 0 },
-});
+const MoviesSchema = new Schema({
+    movieID:{type:String},
+    title :{type:String},
+    studio:{type:String},
+    genres:[{type:String}],
+    directors:[
+        {type:String},
+    ],
+    writers:[{type:String}],
+    actors:[{type:String}],
+    year:{type:Number},
+    length:{type:Number},
+    shortDescription:{type:String},
+    mpaRating:{type:String},
+    criticsRating:{type:Number}
 
-const Recipe = mongoose.model('recipes', recipeSchema);
-// Export the model
-module.exports = Recipe;
+})
+
+const Movie = mongoose.model('Movies',MoviesSchema);
+module.exports = Movie;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,4 @@
-//Student’s Name: Sujal
-// StudentID: 200598524@student.georgianc.on.ca
-// Date: 29 September
-
-// routes/recipes.js
 const express = require('express');
 const router = express.Router();
-
-const recipeController = require('../controllers/RecipesController');
-
-router.get('/', recipeController.GetRecipes);
-
-module.exports = router;
+const movieController = require('../controllers/Controller');
+const {validateMovie}=require('../middleware/middleware');
