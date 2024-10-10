d4c3621d1edc3e058bedead6ed5cb9d946c0d92e
Sujal <sujalkhatri411@gmail.com>
2024-10-10 17:05:35 -0400

    route added to import movies

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,3 +2,6 @@ const express = require('express');
 const router = express.Router();
 const movieController = require('../controllers/Controller');
 const {validateMovie}=require('../middleware/middleware');
+
+//Route to import movies
+router.post('/import',movieController.importMovies);
