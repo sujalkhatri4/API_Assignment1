8b06148e30928e9100c04f1e8cf6b3526ed153d5
Sujal <sujalkhatri411@gmail.com>
2024-10-10 12:34:54 -0400

    chnges in db.js

--- db.js
+++ db.js
@@ -1,20 +1,15 @@
+const mongoose = require('mongoose');
+const MONGO_URI = process.env.MONGO_URI;
 
-const mongoose  = require("mongoose");
-require('dotenv').config();
-
-const mongoURI = process.env.MONGO_URI;
-
-const InitiateMongoServer = async()=>{
+const InitiateMongoServer = async () => {
     try{
-        await mongoose.connect(mongoURI,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-
-    console.log("connected to DB");
-    }catch(e){
+        await mongoose.connect(MONGO_URI);
+        console.log("Connected to DB");
+    }
+    catch(e){
         console.log(e);
-        throw(e);
-        }
-    };
-module.exports= InitiateMongoServer;
+        //throw(e);
+    }
+};
+
+module.exports = InitiateMongoServer;
