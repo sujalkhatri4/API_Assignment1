4c3ebbb1798c973eb810ef49acf35cd0444c7ae9
Sujal <sujalkhatri411@gmail.com>
2024-10-10 16:29:23 -0400

    function add new movie in controller .js

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -13,3 +13,17 @@ exports.getMovies = async(req,res)=>{
             res.status(500).send('Error retrieving Movies');
     }
 };
+
+//Function to create a new movie
+exports.createMovie = async(req,res)=>{
+    try{
+        const newMovie = new Movie(req.body);
+        await newMovie.save();
+         res.status(201).json(newMovie);
+    }
+    catch(e){
+            console.error(e);
+            res.status(500).send('Error creating Movies');
+    }
+};
+
